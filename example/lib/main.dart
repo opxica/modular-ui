2da840bde9f9ebd9c0674c564445e71737b9fa6f
Yash-Khattar <yashkhattar73@gmail.com>
2024-01-05 22:22:12 +0530

    example app added + simple_card

---
+++ example/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:example/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Example',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xFFa2d2ff),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
--- lib/modular_ui.dart
+++ lib/modular_ui.dart
@@ -3,3 +3,4 @@ library modular_ui;
 export 'src/buttons/primary_button.dart';
 export 'src/buttons/loading_button.dart';
 export 'src/cards/primary_card.dart';
+export 'src/cards/simple_card.dart';
