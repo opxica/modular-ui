7dd2cdec37a5b981329d31213bd875b600820255
utkarsh <uttkarsh.dav@gmail.com>
2024-01-08 16:48:49 +0530

    organized exports in modular_ui.dart

--- lib/modular_ui.dart
+++ lib/modular_ui.dart
@@ -1,6 +1,9 @@
 library modular_ui;
 
+/// Breadcrumbs
 export 'src/breadcrumbs/breadcrumbs.dart';
+
+/// Buttons
 export 'src/buttons/gradient_block_level_button.dart';
 export 'src/buttons/gradient_button.dart';
 export 'src/buttons/loading_block_level_button.dart';
@@ -11,18 +14,31 @@ export 'src/buttons/primary_block_level_button.dart';
 export 'src/buttons/primary_button.dart';
 export 'src/buttons/text_block_level_button.dart';
 export 'src/buttons/text_button.dart';
+export 'src/buttons/radio_button.dart';
+
+/// Cards
 export 'src/cards/blog_card.dart';
 export 'src/cards/primary_card.dart';
 export 'src/cards/profile_card.dart';
 export 'src/cards/sign_in_card.dart';
+export 'src/cards/simple_card.dart';
+
+/// checkbox
+export 'src/checkbox/checkbox.dart';
+
+/// Dialogs
+export 'src/dialog/primary_dialog.dart';
+
+/// Footers
 export 'src/footers/logo_footer.dart';
 export 'src/footers/simple_footer.dart';
 export 'src/footers/sitemap_footer.dart';
-export 'src/cards/simple_card.dart';
-export 'src/checkbox/checkbox.dart';
-export 'src/dialog/primary_dialog.dart';
+
+/// Input Fields
 export 'src/input_fields/primary_input_field.dart';
+
+/// Rating bars
 export 'src/ratingbar/ratingbar.dart';
+
+/// Switch
 export 'src/switch/switch.dart';
-export 'src/cards/blog_card.dart';
-export 'src/buttons/radio_button.dart';
