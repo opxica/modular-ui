0366aae6a940321ba27580accd7df401fca69803
Joscha Peter <github@oxelf.com>
2024-01-08 19:46:20 +0100

    Added tabs component.

---
+++ example/lib/components/tabs.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:modular_ui/modular_ui.dart';
+
+Widget tabsComponent() {
+  ValueNotifier<int> tabIndexNotifier = ValueNotifier(0);
+  ValueNotifier<int> transparentTabIndexNotifier = ValueNotifier(0);
+  ValueNotifier<int> underlinedTabIndexNotifier = ValueNotifier(0);
+  ValueNotifier<int> verticalTabIndexNotifier = ValueNotifier(0);
+  return Container(
+    color: Color.fromARGB(255, 248, 250, 252),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Default"),
+        ValueListenableBuilder(
+            valueListenable: tabIndexNotifier,
+            builder: (context, tabIndex, child) {
+              return MUITabs(
+                tabIndex: tabIndex,
+                onChanged: (index) {
+                  tabIndexNotifier.value = index;
+                },
+                tabs: [
+                  MUITab(label: "HTML"),
+                  MUITab(label: "React"),
+                  MUITab(label: "Vue"),
+                  MUITab(label: "Angular"),
+                  MUITab(label: "Svelte"),
+                ],
+              );
+            }),
+        SizedBox(
+          height: 30,
+        ),
+        Text("Transparent"),
+        ValueListenableBuilder(
+            valueListenable: transparentTabIndexNotifier,
+            builder: (context, tIndex, child) {
+              return MUITabs.transparent(
+                tabIndex: tIndex,
+                onChanged: (index) {
+                  transparentTabIndexNotifier.value = index;
+                },
+                tabs: [
+                  MUITab(label: "HTML"),
+                  MUITab(label: "React"),
+                  MUITab(label: "Vue"),
+                  MUITab(label: "Angular"),
+                  MUITab(label: "Svelte"),
+                ],
+              );
+            }),
+        SizedBox(
+          height: 30,
+        ),
+        Text("Underlined"),
+        ValueListenableBuilder(
+            valueListenable: underlinedTabIndexNotifier,
+            builder: (context, tIndex, child) {
+              return MUITabs.underlined(
+                tabIndex: tIndex,
+                onChanged: (index) {
+                  underlinedTabIndexNotifier.value = index;
+                },
+                tabs: [
+                  MUITab(label: "HTML"),
+                  MUITab(label: "React"),
+                  MUITab(label: "Vue"),
+                  MUITab(label: "Angular"),
+                  MUITab(label: "Svelte"),
+                ],
+              );
+            }),
+        SizedBox(
+          height: 30,
+        ),
+        Text("Vertical"),
+        ValueListenableBuilder(
+            valueListenable: verticalTabIndexNotifier,
+            builder: (context, tabIndex, child) {
+              return MUITabs(
+                axis: Axis.vertical,
+                tabIndex: tabIndex,
+                onChanged: (index) {
+                  verticalTabIndexNotifier.value = index;
+                },
+                underline: false,
+                tabs: [
+                  MUITab(label: "HTML"),
+                  MUITab(label: "React"),
+                  MUITab(label: "Vue"),
+                  MUITab(label: "Angular"),
+                  MUITab(label: "Svelte"),
+                ],
+              );
+            }),
+      ],
+    ),
+  );
+}
--- example/lib/home.dart
+++ example/lib/home.dart
@@ -4,6 +4,7 @@ import 'package:example/components/cards.dart';
 import 'package:example/components/checkbox.dart';
 import 'package:example/components/empty.dart';
 import 'package:example/components/switch.dart';
+import 'package:example/components/tabs.dart';
 import 'package:flutter/material.dart';
 
 import 'components/dialog.dart';
@@ -67,6 +68,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
         'name': 'Avatar',
         'route': avatarComponent(),
       },
+      {
+        "name": "Tabs",
+        "route": tabsComponent(),
+      }
     ];
     _tabController = TabController(length: components.length, vsync: this);
   }
