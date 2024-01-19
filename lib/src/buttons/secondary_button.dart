983678d8260f21b27ff0b9cd5a896f3a9d408144
utkarsh <uttkarsh.dav@gmail.com>
2024-01-20 00:18:52 +0530

    created secondary buttons

--- lib/src/buttons/secondary_button.dart
+++ lib/src/buttons/secondary_button.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:modular_ui/src/utils/dimensions.dart';
 
-/// A customizable primary button by ModularUI with optional icons.
-class MUIPrimaryButton extends StatefulWidget {
-  const MUIPrimaryButton({
+/// A customizable Secondary button by ModularUI with optional icons.
+class MUISeconaryButton extends StatefulWidget {
+  const MUISeconaryButton({
     Key? key,
     required this.text,
     required this.onPressed,
-    this.bgColor = Colors.black,
+    this.boxShadows,
+    this.bgColor = const Color(0xff212123),
+    this.tappedBgColor = const Color.fromARGB(255, 22, 22, 24),
     this.textColor = Colors.white,
     this.borderRadius = 10,
     this.hapticsEnabled = false,
     this.animationDuration = 250,
-    this.widthFactorUnpressed = 0.04,
-    this.widthFactorPressed = 0.035,
-    this.heightFactorUnPressed = 0.03,
-    this.heightFactorPressed = 0.03,
-    this.maxHorizontalPadding = 70,
     this.leadingIcon,
     this.actionIcon,
     this.iconColor = Colors.white,
@@ -26,13 +22,16 @@ class MUIPrimaryButton extends StatefulWidget {
   /// The Text to display inside the button.
   final String text;
 
-  /// Background Color of The Primary Button, default: black.
+  /// Background Color of The Secondary Button
   final Color bgColor;
 
-  /// Text Color of the Primary Button, default: white.
+  /// Background Color of The Secondary Button when tapped.
+  final Color tappedBgColor;
+
+  /// Text Color of the Secondary Button, default: white.
   final Color textColor;
 
-  /// Border Radius for Primary Button, default: 10.
+  /// Border Radius for Secondary Button, default: 10.
   final double borderRadius;
 
   /// Animation Duration in Milliseconds, default: 250 ms.
@@ -40,22 +39,6 @@ class MUIPrimaryButton extends StatefulWidget {
 
   /// Enables Light Haptic Feedback.
   final bool hapticsEnabled;
-
-  /// A double value which gets multiplied by the current screen width when the button is not pressed.
-  final double widthFactorUnpressed;
-
-  /// A double value which gets multiplied by the current screen width when the button is pressed.
-  final double widthFactorPressed;
-
-  /// A double value which gets multiplied by the current screen height when the button is pressed.
-  final double heightFactorPressed;
-
-  /// A double value which gets multiplied by the current screen height when the button is not pressed.
-  final double heightFactorUnPressed;
-
-  /// A double value which determines maximum horizontal padding a button can accumulate
-  /// Play with this value if you want to use the button on a larger screen size
-  final double maxHorizontalPadding;
 
   /// Optional leading icon for the button.
   final IconData? leadingIcon;
@@ -69,19 +52,22 @@ class MUIPrimaryButton extends StatefulWidget {
   /// On Pressed Function.
   final VoidCallback onPressed;
 
+  /// Box shadows for button
+  final List<BoxShadow>? boxShadows;
+
   @override
-  State<MUIPrimaryButton> createState() => _PrimaryButtonState();
+  State<MUISeconaryButton> createState() => _MUISecondaryButtonState();
 }
 
-class _PrimaryButtonState extends State<MUIPrimaryButton> {
-  bool _isPrimaryButtonPressed = false;
+class _MUISecondaryButtonState extends State<MUISeconaryButton> {
+  bool _isSecondaryButtonPressed = false;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTapDown: (_) {
         setState(() {
-          _isPrimaryButtonPressed = true;
+          _isSecondaryButtonPressed = true;
         });
         if (widget.hapticsEnabled) {
           HapticFeedback.lightImpact();
@@ -90,37 +76,23 @@ class _PrimaryButtonState extends State<MUIPrimaryButton> {
       },
       onTapUp: (_) {
         setState(() {
-          _isPrimaryButtonPressed = false;
+          _isSecondaryButtonPressed = false;
         });
       },
       onTapCancel: () {
         setState(() {
-          _isPrimaryButtonPressed = false;
+          _isSecondaryButtonPressed = false;
         });
       },
       child: AnimatedContainer(
         duration: Duration(milliseconds: widget.animationDuration),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(widget.borderRadius),
-          color: widget.bgColor,
+          color:
+              _isSecondaryButtonPressed ? widget.tappedBgColor : widget.bgColor,
+          boxShadow: widget.boxShadows
         ),
-        padding: EdgeInsets.symmetric(
-          horizontal: _isPrimaryButtonPressed
-              ? getScreenWidth(context) * widget.widthFactorPressed
-              : getScreenWidth(context) * widget.widthFactorUnpressed,
-          vertical: _isPrimaryButtonPressed
-              ? getScreenWidth(context) * widget.heightFactorPressed
-              : getScreenWidth(context) * widget.heightFactorUnPressed,
-        ).clamp(
-          const EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 8,
-          ),
-          EdgeInsets.symmetric(
-            horizontal: widget.maxHorizontalPadding,
-            vertical: 16,
-          ),
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -135,7 +107,7 @@ class _PrimaryButtonState extends State<MUIPrimaryButton> {
               widget.text,
               style: TextStyle(
                 color: widget.textColor,
-                fontWeight: FontWeight.bold,
+                fontWeight: FontWeight.w500,
               ),
             ),
             SizedBox(width: widget.actionIcon != null ? 8.0 : 0.0),
