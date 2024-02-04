2ccb30b1ae3f14a07da4a5536f34ef781494e41d
Yash-Khattar <yashkhattar73@gmail.com>
2024-02-05 00:57:21 +0530

    carousel typo

--- example/lib/components/rating_bar.dart
+++ example/lib/components/rating_bar.dart
@@ -2,14 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:modular_ui/modular_ui.dart';
 
 Widget ratingBar() {
-  return const Column(
+  return Column(
     mainAxisAlignment: MainAxisAlignment.center,
     children: [
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           MUIRatingBar(
-            selectedCount: 4,
+            selectedCount: 3,
+            icon: Icons.star_rounded,
+            iconOutlined: Icons.star_border_rounded,
+            borderColor: Colors.grey,
+            selectedColor: Colors.orange,
+            itemCount: 5,
+            size: 24,
+            onChanged: (count) {
+              // Handle rating change
+            },
           ),
         ],
       ),
